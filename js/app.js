c2d828c74cce92bbfec53f46593f6e2e8f2f52cf
Andreas Thaler <andi@hoehenluft.com>
2018-05-17 17:33:23 +0200

    Updated js/app.js to display enemy/player

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(move, x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.move = move;
+    this.x = x;
+    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.move * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +27,34 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (move, x, y) {
+  this.move = move;
+  this.x = x;
+  this.y = y;
+  // The image/sprite for our player
+  this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function() {
+}
 
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(pressedKey) {
+  if (pressedKey == 'left') player.x -= player.move;
+  if (pressedKey == 'right') player.x += player.move;
+  if (pressedKey == 'up') player.y -= player.move;
+  if (pressedKey == 'down') player.y += player.move;
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player(95, 0, 400);
+var enemy = new Enemy(30, 0, 400);
+allEnemies.push(enemy);
 
 
 // This listens for key presses and sends the keys to your
